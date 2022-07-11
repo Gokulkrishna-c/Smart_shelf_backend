9da8116a8526fafcb8706eca3af9f43d2a64256b
Gokul Krishna <74834824+Gokulkrishna-c@users.noreply.github.com>
2022-07-12 00:48:43 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+var output = ""
+const bodyparser = require("body-parser")
+const exphbs  = require("express-handlebars")
+const hbs = exphbs.create({
+  extname:'.hbs',
+})
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+app.set('views', './views');
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.json())
+app.get("/",(req,res)=>{
+    res.send("hello wprdas as")
+})
+app.get("/weights",(req,res)=>{
+})
+app.post("/weight",(req,res)=>{
+    const weight = req.body.weight;
+    // res.send(weight)
+    res.render("output",{weight:weight})
+})
+app.listen(port,()=>{
+    console.log(`app running  in ${port}`);
+})
