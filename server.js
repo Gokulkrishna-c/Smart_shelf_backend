208aac8776182b3fa42b366c0cac39f2a32e4dfc
Gokul Krishna <74834824+Gokulkrishna-c@users.noreply.github.com>
2022-07-28 00:21:24 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose")
+const app = express();
+const dotenv = require("dotenv")
+const port = process.env.PORT || 8080;
+const bodyparser = require("body-parser")
+const postRoutes = require("./routes/weight.js")
+const cors = require("cors")
+dotenv.config()
+var output = 0
+app.use(cors())
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.json())
+app.use("/weight", postRoutes);
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+
+mongoose
+  .connect("mongodb+srv://lolan:lolan@cluster0.ke01k.mongodb.net/?retryWrites=true&w=majority")
+  .then(() =>
+    app.listen(port, () => console.log(`server running on port ${port}`))
+  )
+  .catch((err) => console.log(err.message));
